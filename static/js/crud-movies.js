1615d897e38fe4a81dc6e3c1d51c5032b31fcf4c
Fede Liquin NHP <josefliquin@gmail.com>
2023-12-29 11:37:33 -0300

    Se agrega crud-movies con javacript vanilla y que se guarda al localstorage

--- static/js/crud-movies.js
+++ static/js/crud-movies.js
@@ -41,125 +41,174 @@ function showMoviesNodes(){
         // Agregar la fila a la tabla
         tableMovies.appendChild(tr);
     });
-  }
+}
   
-  /**
-   * Funcion que permite crear un elemento <tr> para la tabla de peliculas
-   * por medio del uso de template string de JS.
-   */
-  function showMoviesTemplate(){
-    let movies = JSON.parse(localStorage.getItem('movies'));
-    const tableMovies = document.querySelector('#list-table-movies tbody');
-    tableMovies.innerHTML='';
-    movies.forEach((movie, index) => {
-      let tr = `<tr>
-                    <td>${movie.title}</td>
-                    <td>${movie.director}</td>
-                    <td>${movie.release_date}</td>
-                    <td><img src="${movie.banner}" alt="${movie.title}" width="30%"></td>
-                    <td><button class="btn-cac" onclick='deleteMovieAlert(${index})'><i class="fa fa-trash" ></button></i></td>
-                  </tr>`;
-      tableMovies.insertAdjacentHTML("beforeend",tr);
-    });
-  }
+/**
+ * Funcion que permite crear un elemento <tr> para la tabla de peliculas
+ * por medio del uso de template string de JS.
+ */
+function showMoviesTemplate(){
+  let movies = JSON.parse(localStorage.getItem('movies'));
+  const tableMovies = document.querySelector('#list-table-movies tbody');
+  tableMovies.innerHTML='';
+  movies.forEach((movie, index) => {
+    let tr = `<tr>
+                  <td>${movie.title}</td>
+                  <td>${movie.director}</td>
+                  <td>${movie.release_date}</td>
+                  <td><img src="${movie.banner}" alt="${movie.title}" width="30%"></td>
+                  <td>
+                    <button class="btn-cac" onclick='updateMovie(${movie.id})'><i class="fa fa-pencil" ></button></i>
+                    <button class="btn-cac" onclick='deleteMovieAlert(${movie.id})'><i class="fa fa-trash" ></button></i>
+                    </td>
+                </tr>`;
+    tableMovies.insertAdjacentHTML("beforeend",tr);
+  });
+}
   
-  /**
-   * funcion que permite agregar una pelicula al listado de peliculas
-   * almacenado en el localstorage
-   */
-  function addMovie(){
-    const inputTitle = document.querySelector('#title');
-    const inputDirector = document.querySelector('#director');
-    const inputReleaseDate = document.querySelector('#release-date');
-    const inputBanner = document.querySelector('#banner-form');
+/**
+ * funcion que permite agregar o modificar una pelicula al listado de peliculas
+ * almacenado en el localstorage
+ */
+function saveMovie(){
+  const form = document.querySelector('#form-movie');
 
-  
-    if(inputTitle.value !== '' && inputDirector.value !=='' && inputReleaseDate.value !=='' && inputBanner.value !==''){
-      //Obtiene el listado de peliculas del localstorage, en caso de no existir crea una array vacio
-      let movies = JSON.parse(localStorage.getItem('movies')) || [];
+  const inputId = document.querySelector('#id-movie');
+  const inputTitle = document.querySelector('#title');
+  const inputDirector = document.querySelector('#director');
+  const inputReleaseDate = document.querySelector('#release-date');
+  const inputBanner = document.querySelector('#banner-form');
+
+  if(inputTitle.value !== '' && inputDirector.value !=='' && inputReleaseDate.value !=='' && inputBanner.value !==''){
+    //Obtiene el listado de peliculas del localstorage, en caso de no existir crea una array vacio
+    let movies = JSON.parse(localStorage.getItem('movies')) || [];
+    
+    if(inputId.value!==""){
+      let movieFind = movies.find(movie => movie.id == inputId.value);
+      if (movieFind) {
+        movieFind.title = inputTitle.value;
+        movieFind.director = inputDirector.value;
+        movieFind.release_date = inputReleaseDate.value;
+        movieFind.banner = inputBanner.value;
+      }
+    }else{
       let newMovie = {
+        id: Object.keys(movies).length+1,
         title: inputTitle.value,
         director: inputDirector.value,
         release_date: inputReleaseDate.value,
         banner: inputBanner.value,
       }
       movies.push(newMovie);
-      //Se actualiza el array de peliculas en el localstorage
-      localStorage.setItem('movies',JSON.stringify(movies));
-      showMoviesTemplate();
-      //Limpieza de los campos del formulario
-      inputTitle.value='';
-      inputDirector.value='';
-      inputReleaseDate.value='';
-      inputBanner.value='';
-        Swal.fire({
-            title: 'Exito!',
-            text: 'Pelicula agregada exitosamente.',
-            icon: 'success',
-            confirmButtonText: 'Cerrar'
-        })
-    }else{
-        Swal.fire({
-            title: 'Error!',
-            text: 'Por favor completa todos los campos.',
-            icon: 'error',
-            confirmButtonText: 'Cerrar'
-        });
     }
+    //Se actualiza el array de peliculas en el localstorage
+    localStorage.setItem('movies',JSON.stringify(movies));
+    showMoviesTemplate();
+    //Limpieza de los campos del formulario
+    // inputTitle.value='';
+    // inputDirector.value='';
+    // inputReleaseDate.value='';
+    // inputBanner.value='';
+    form.reset();
+    Swal.fire({
+        title: 'Exito!',
+        text: 'Operacion exitosa.',
+        icon: 'success',
+        confirmButtonText: 'Cerrar'
+    })
+  }else{
+    Swal.fire({
+        title: 'Error!',
+        text: 'Por favor completa todos los campos.',
+        icon: 'error',
+        confirmButtonText: 'Cerrar'
+    });
   }
-  
-  /**
-   * Function que permite eliminar una pelicula del array del localstorage
-   * de acuedo al indice del mismo
-   * @param {number} index posición del array que se va a eliminar
-   */
-  function deleteMovie(index){
-    let movies = JSON.parse(localStorage.getItem('movies'));
-    if(movies[index]!=='undefined'){
-      movies.splice(index,1);
-      localStorage.setItem('movies',JSON.stringify(movies));
-      showMoviesTemplate();
-    }
-  }
+}
 
-  /**
-   * Function que permite eliminar una pelicula del array del localstorage
-   * de acuedo al indice del mismo por medio de sweet alert
-   * @param {number} index posición del array que se va a eliminar
-   */
-  function deleteMovieAlert(index){
-    let movies = JSON.parse(localStorage.getItem('movies'));
-    
-    if(movies[index]!=='undefined'){
-        Swal.fire({
-            title: "Esta seguro de eliminar la pelicula?",
-            showCancelButton: true,
-            confirmButtonText: "Eliminar",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                movies.splice(index,1);
-                localStorage.setItem('movies',JSON.stringify(movies));
-                showMoviesTemplate();
-                Swal.fire("Pelicula Eliminada!", "", "success");
-            }
-        });
-    }else{
-        Swal.fire({
-            title: 'Error!',
-            text: 'No se puede eliminar la pelicula.',
-            icon: 'error',
-            confirmButtonText: 'Cerrar'
-        })
-    }
+/**
+ * Function que permite cargar el formulario para editar una pelicula
+ * de acuedo al id de la pelicula
+ * @param {number} movieId id movie que se va a actualizar
+ */
+function updateMovie(movieId){
+  let movies = JSON.parse(localStorage.getItem('movies'));
+  //se utiliza el metodo find para poder asegurarnos que exista una pelicula con el id que queremos eliminar.
+  let movieToUpdate = movies.find(movie => movie.id===movieId);
+  if(movieToUpdate){
+    const inputId = document.querySelector('#id-movie');
+    const inputTitle = document.querySelector('#title');
+    const inputDirector = document.querySelector('#director');
+    const inputReleaseDate = document.querySelector('#release-date');
+    const inputBanner = document.querySelector('#banner-form');
+    inputId.value = movieToUpdate.id;
+    inputTitle.value = movieToUpdate.title;
+    inputDirector.value = movieToUpdate.director;
+    inputReleaseDate.value = movieToUpdate.release_date;
+    inputBanner.value = movieToUpdate.banner;
   }
-  
-  
-  //Agregar eventos a elementos una vez que contenido haya sido cargado en el DOM
-  document.addEventListener('DOMContentLoaded', function() {
-    const btnAddMovie = document.querySelector('#btn-add-movie');
-    btnAddMovie.addEventListener('click',addMovie);
-  });
-  
+}
+
+/**
+ * Function que permite eliminar una pelicula del array del localstorage
+ * de acuedo al indice del mismo
+ * @param {number} movieId id movie que se va a eliminar
+ */
+function deleteMovie(movieId){
+  let movies = JSON.parse(localStorage.getItem('movies'));
+  //se utiliza el metodo find para poder asegurarnos que exista una pelicula con el id que queremos eliminar.
+  let movieToDelete = movies.find(movie => movie.id===movieId);
+  if(movieToDelete){
+    //se utiliza el metodo filter para actualizar el array de movies, sin tener el elemento encontrado en cuestion.
+    movies = movies.filter(movie => movie.id !== movieToDelete.id);
+    //se actualiza el localstorage
+    localStorage.setItem('movies',JSON.stringify(movies));
+    showMoviesTemplate();
+  }
+}
+
+/**
+ * Function que permite eliminar una pelicula del array del localstorage
+ * de acuedo al indice del mismo por medio de sweet alert
+ * @param {number} movieId id movie que se va a eliminar
+ */
+function deleteMovieAlert(movieId){
+  let movies = JSON.parse(localStorage.getItem('movies'));
+  //se utiliza el metodo find para poder asegurarnos que exista una pelicula con el id que queremos eliminar.
+  let movieToDelete = movies.find(movie => movie.id===movieId); 
+
+  if(movieToDelete){
+      Swal.fire({
+          title: "Esta seguro de eliminar la pelicula?",
+          showCancelButton: true,
+          confirmButtonText: "Eliminar",
+      }).then((result) => {
+          if (result.isConfirmed) {
+            //se utiliza el metodo filter para actualizar el array de movies, sin tener el elemento encontrado en cuestion.
+            movies = movies.filter(movie => movie.id !== movieToDelete.id);
+            //se actualiza el localstorage
+            localStorage.setItem('movies',JSON.stringify(movies));
+            showMoviesTemplate();
+            Swal.fire("Pelicula Eliminada!", "", "success");
+          }
+      });
+  }else{
+      Swal.fire({
+          title: 'Error!',
+          text: 'No se puede eliminar la pelicula.',
+          icon: 'error',
+          confirmButtonText: 'Cerrar'
+      })
+  }
+}
+
+
+//Agregar eventos a elementos una vez que contenido haya sido cargado en el DOM
+document.addEventListener('DOMContentLoaded', function() {
+  const btnSaveMovie = document.querySelector('#btn-save-movie');
+  btnSaveMovie.addEventListener('click',saveMovie);
+});
+
 //   showMoviesNodes();
-  showMoviesTemplate();
+showMoviesTemplate();
   
